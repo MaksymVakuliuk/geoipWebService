afbef4b11344acbbfed2eb8d123f46971f0728f5
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-09-24 20:52:33 +0300

    default Spring Boot congiguration with check style plugin add

---
+++ src/main/java/geo/ip/web/service/geoipwebservice/GeoipWebServiceApplication.java
@@ -0,0 +1,13 @@
+package geo.ip.web.service.geoipwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoipWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoipWebServiceApplication.class, args);
+    }
+
+}
